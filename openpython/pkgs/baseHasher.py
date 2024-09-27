6ea29bef75e18037ed611ce7d9d658afaeb00e4c
CreatorCing <kleevstotheo@gmail.com>
2024-09-26 20:52:54 -0400

    Modified Package P1

--- openpython/pkgs/baseHasher.py
+++ openpython/pkgs/baseHasher.py
@@ -1,8 +1,10 @@
 import base64
+import random
 import os
 
 def encode(data):
-    key = base64.b64decode(bytes(data, 'utf-8'))
+    randEnc = random.randint(50, 100)
+    key = base64.b64encode(bytes(data, 'utf-8'))
     return key
 
 def decode(key):
