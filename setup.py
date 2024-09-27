a00eee849d10cfa46a0fdfebc968ee3b368a393a
ThatCasualDonut <cchandler64@learn.flvs.net>
2024-09-27 14:56:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openpy-pkg',
-    version='0.1.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         "colorama"
